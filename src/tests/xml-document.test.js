c29f3b127bcd11218bc5ce7eae071ffbf0fcf78d
CJ Bailey <cj.bailey@greedygoblingames.co.uk>
2019-05-08 22:43:11 +0100

    Self-terminating tags are parsed correctly

--- src/tests/xml-document.test.js
+++ src/tests/xml-document.test.js
@@ -2,27 +2,39 @@ import XmlDocument from "../XmlDocument";
 import XmlElement from "../XmlElement";
 
 it("XmlDocument renders correctly", () => {
-  let xml = "<root lang='en'><foo a='qwe'><foobar>hello world</foobar></foo></root>";
-  let xmlDoc = new XmlDocument(xml);
+	let xml = "<root lang='en'><foo a='qwe'><foobar>hello world</foobar></foo></root>";
+	let xmlDoc = new XmlDocument(xml);
 
-  expect(xmlDoc.toString()).toBe(xml);
+	expect(xmlDoc.toString()).toBe(xml);
 });
 
 it("XmlDocument cannot contain multiple root nodes", () => {
-  let xml = "<root lang='en'><foo ref='qwe' ref2='123'><foobar></foobar></foo></root>";
-  let xmlDoc = new XmlDocument(xml);
+	let xml = "<root lang='en'><foo ref='qwe' ref2='123'><foobar></foobar></foo></root>";
+	let xmlDoc = new XmlDocument(xml);
 
-  expect(xmlDoc.toString()).toBe(xml);
+	expect(xmlDoc.toString()).toBe(xml);
 
-  xmlDoc.append(new XmlElement("invalid"));
-  expect(xmlDoc.toString()).toBe(xml);
+	xmlDoc.append(new XmlElement("invalid"));
+	expect(xmlDoc.toString()).toBe(xml);
 });
 
 it("<!DOCTYPE html> is invalid XML", () => {
-  let xml = "<!DOCTYPE html><root><foo>1234</foo></root>";
-  expect(() => new XmlDocument(xml)).toThrow("Error while parsing XML string");
+	let xml = "<!DOCTYPE html><root><foo>1234</foo></root>";
+	expect(() => new XmlDocument(xml)).toThrow("Error while parsing XML string");
 });
 
 it("<?xml ... ?> attributes are included in XmlDocument attributes", () => {
-  let xml = '<?xml version="1.0" encoding="UTF-8"?><root></root>';
+	let xml = '<?xml version="1.0" encoding="UTF-8"?><root></root>';
+	let xmlDoc = new XmlDocument(xml);
+
+	expect(xmlDoc.attributes.filter(x => x.name === "version" && x.value === "1.0").length === 1).toBe(true);
+	expect(xmlDoc.attributes.filter(x => x.name === "encoding" && x.value === "UTF-8").length === 1).toBe(true);
+});
+
+it("Self-terminating tags are parsed correctly", () => {
+	let xmlInput = "<root><foo abc='1234'><bar/></foo></root>";
+	let xmlDoc = new XmlDocument(xmlInput);
+	let xmlOutput = "<root><foo abc='1234'><bar></bar></foo></root>";
+
+	expect(xmlDoc.toString()).toBe(xmlOutput);
 });
