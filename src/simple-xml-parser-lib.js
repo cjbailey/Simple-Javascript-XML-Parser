04f0f24f4e38e64ba4642e43773757b50e30020e
CJ Bailey <chris.bailey@jcb.com>
2019-05-16 16:06:21 +0100

    Can now build just the library elements of the project

---
+++ src/simple-xml-parser-lib.js
@@ -0,0 +1,7 @@
+// This is used for building just the XML parser library elements of the project rather than the full standalone website.
+import XmlAttribute from "./XmlAttribute";
+import XmlElement from "./XmlElement";
+import XmlTextContent from "./XmlTextContent";
+import XmlDocument from "./XmlDocument";
+
+export { XmlAttribute, XmlElement, XmlTextContent, XmlDocument };
